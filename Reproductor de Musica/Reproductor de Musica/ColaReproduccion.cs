0709414f637d54791598aed5698e12487bfdc650
Caleb Aguilar <jocalebaguilar@gmail.com>
2026-03-19 19:20:46 -0600

    Añadi la clase ColaReproduccion

---
+++ Reproductor de Musica/Reproductor de Musica/ColaReproduccion.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reproductor_de_Musica
+{
+    public class ColaReproduccion
+    {
+        private NodoCancion frente;
+        private NodoCancion final;
+
+        //metodo constructor
+        public ColaReproduccion()
+        {
+            frente = null;
+            final = null;
+        }
+
+        //verificar si la cola esta vacia
+        public bool EstaVacia()
+        {
+            if (frente == null)
+                return true;
+            else
+                return false;
+        }
+
+        //agregar una cancion a la cola
+        public void Encolar(Cancion cancion)
+        {
+            NodoCancion nuevo = new NodoCancion(cancion);
+
+            if (EstaVacia())
+                frente = final = nuevo;
+            else
+            {
+                final.Siguiente = nuevo;
+                nuevo.Anterior = final;
+                final = nuevo;
+            }
+        }
+
+        //eliminar una cancion de la cola
+        public Cancion Desencolar()
+        {
+            if (EstaVacia())
+                return null;
+
+            else 
+            {
+                NodoCancion aux = frente;
+                Cancion c = aux.Dato;
+
+                frente = frente.Siguiente;
+
+                if (frente == null)
+                    final = null;
+                else
+                    frente.Anterior = null;
+
+                return c;
+            }
+        }
+    }
+}
